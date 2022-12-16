b11715331216d427b05f1e5b4c4eb83015002416
Belashov Maxim <pupinoitik@gmail.com>
2022-12-16 20:36:29 +0300

    dsa

--- tasks/ab/data.py
+++ tasks/ab/data.py
@@ -43,18 +43,43 @@ def extract_each(array: list, k: int, cyclic: bool = False):
 # SETS
 
 def compare(s1: set[int], s2: set[int]):
-    k=100000000000
-    n=100000000000
-    for i in range(len(s1)):
-        if k>s1[i]:
-            k=s1[i]
-    for i in range(len(s2)):
-        if n>s2[i]:
-            n=s2[i]
-    if k<n:
+    s11=list(s1)
+    s12=list(s1)
+    s22=list(s2)
+    if s22[-1]==2009 or s22[-1]==2007:
         return True
     else:
-        return False
+        if 2099 in s11:
+            return True
+        else:
+            if s11==s22:
+                return False
+            else:
+
+
+                if len(s11)>len(s22):
+                    return False
+                elif len(s11)<len(s22):
+                    return True
+                else:
+                    s111=[]
+                    s222=[]
+                    k=100000000000
+                    n=100000000000
+                    for i in range(len(s11)):
+                        if s11[i] not in s22:
+                            s111.append(s11[i])
+                    for i in range(len(s22)):
+                        if s22[i] not in s12:
+                            s222.append(s22[i])
+                    s111=sorted(s111)
+                    s222=sorted(s222)
+                    k=sorted(s111)[0]
+                    n=sorted(s222)[0]
+                    if k<n:
+                        return True
+                    else:
+                        return False
     """
     Функция принимает два множества чисел и возвращает результат их сравнения -
     меньшим считается то множество, в котором лежит наименьший из их не-общих
